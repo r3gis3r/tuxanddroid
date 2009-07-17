dbfa2cedd16e3aaadbb5324e9f6e2dfdfc70dec4
r3gis <r3gis@localhost>
2009-07-17 13:28:11 +0200

    (no commit message)

--- TuxAndDroid/src/fr/r3gis/TuxAndDroid/views/TuxPreferences.java
+++ TuxAndDroid/src/fr/r3gis/TuxAndDroid/views/TuxPreferences.java
@@ -1,13 +1,25 @@
 package fr.r3gis.TuxAndDroid.views;
 
 import fr.r3gis.TuxAndDroid.R;
+import fr.r3gis.TuxAndDroid.service.ApiConnector;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
 public class TuxPreferences extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		
+		Log.i("TuxPrefs", "Preferences started");
+	}
+	
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+		
+		Log.i("TuxPrefs", "Prefs stopped, reconnect to server");
+		ApiConnector.singleton.connectToServer();
 	}
 }
